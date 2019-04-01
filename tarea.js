23c510dc407ecd48b5fe5448eab8ab4c3a8ab817
luie1 <lruise.1@gmail.com>
2019-04-01 17:11:59 -0400

    pregunta 3 a medias

--- tarea.js
+++ tarea.js
@@ -14,4 +14,39 @@ function cinco(p,n){
     return p;
 }
 
-console.log(cinco(5,1));
+function tres(cad){
+    var res='';
+    var res2='';
+    var i=0;
+    while(i<cad.length){
+        let c=cad.charAt(i);
+        if(c=='('){
+          i++;
+          while(c!=')'){
+            c=cad.charAt(i);
+            if(c!=')'){
+              res2+=c;
+              i++;
+            }
+          }
+          res2=volteo(res2);
+          res+=res2;
+        }else{
+          res+=c;
+        }
+        i++;
+    }
+    return res;
+}
+
+function volteo(cad){
+  var res='';
+  for(var i=cad.length-1;i>=0;i--){
+    let c=cad.charAt(i);
+    res+=c;
+  }
+  return res;
+}
+
+//console.log(cinco(5,1));
+console.log(tres("foo(bar)baz"));
